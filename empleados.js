e4fb50b33129dc4d058fa55e2dc1fe7d734ca0cd
FrankOrtegon <frank.ortegon@sofka.com.co>
2023-10-20 20:45:32 -0500

    Se realiza funcionalidad para agregar empleados

---
+++ empleados.js
@@ -0,0 +1,23 @@
+let empleados = [];
+
+function Empleado(nombre, apellido, fechaNacimiento, edad, cargo){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.fechaNacimiento = fechaNacimiento;
+    this.edad = edad;
+    this.cargo = cargo;
+}
+
+function agregarEmpleado(){
+
+    let nombre = document.getElementById("txtNombre").value;
+    let apellido = document.getElementById("txtApellido").value;
+    let fechaNacimiento = document.getElementById("txtNacimiento").value;
+    let edad = document.getElementById("txtEdad").value;
+    let cargo = document.getElementById("txtCargo").value;
+
+    let empleado = new Empleado(nombre,apellido,fechaNacimiento, edad,cargo);
+    empleados.push(empleado);
+
+    alert("Empleado ha sido registrado");
+}
